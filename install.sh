79b0a6537df61b1282bdba7a632eca7bcbfe8d54
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-02-27 21:25:03 -0300

    changes to make script compatible with pacman

--- install.sh
+++ install.sh
@@ -2,20 +2,25 @@
 
 # Author: Tom Hudson
 
+# Install keyring to avoid signature problems
+sudo pacman -S manjaro-keyring
+
 # Update and upgrade distro
+sudo pacman -Syu
+sudo pacman -Suu
 
 function install {
   which $1 &> /dev/null
 
   if [ $? -ne 0 ]; then
     echo "Installing: ${1}..."
-    apt-get install $1
+    pacman -S install $1
   else
     echo "Already installed: ${1}"
   fi
 }
 
-# dev stuff
+# Base Dev 
 install vim
 install tmux
 
